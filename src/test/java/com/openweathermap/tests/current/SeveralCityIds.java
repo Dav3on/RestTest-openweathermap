089888b7a0f307c0e90bb3929bd1b937229f9169
Andrey Kulishov <a_kulishov@wargaming.net>
2016-05-25 19:40:02 +0300

    Added small changes to SeveralCityIds test class

--- src/test/java/com/openweathermap/tests/current/SeveralCityIds.java
+++ src/test/java/com/openweathermap/tests/current/SeveralCityIds.java
@@ -1,5 +1,46 @@
 package com.openweathermap.tests.current;
 
+import org.junit.After;
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+import static com.openweathermap.Cities.CITIES;
+import static com.openweathermap.Common.API_KEY;
+import static com.openweathermap.Common.BASE_API_URL;
+import static com.openweathermap.Common.getRandomCity;
+
 //http://openweathermap.org/current#severalid
 public class SeveralCityIds {
+    public final String endpointURL = BASE_API_URL+"/group?appid="+API_KEY+"&";
+    ArrayList<HashMap> arr = new ArrayList<HashMap>();
+    String arrayOfIds;
+
+    @Before
+    public void setUp()
+    {
+        Integer counter = new Random().nextInt(CITIES.size())+1;
+
+        for (int i=0; i<counter; i++){
+            arr.add(getRandomCity());
+        }
+
+        arrayOfIds = getStringOfIds();
+    }
+
+    public String getStringOfIds(){
+        String temp="";
+        for (HashMap map: this.arr){
+            temp += map.get("id").toString()+",";
+        }
+        return temp;
+    }
+
+    @After
+    public void tearDown() {
+        arr = null;
+        arrayOfIds = null;
+    }
 }
